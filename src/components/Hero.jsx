fa3e097d3d8a5e75006ce6da4b930cfa1918dd23
matiasvidal9 <matias98vidal@hotmail.com>
2026-01-25 21:26:19 -0300

    se arreglo problema en app.jsx para que renderise en vite

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,15 +3,14 @@ import heroImg from "../assets/heroImg.png";
 import ItemListContainer from "./ItemListContainer";
 
 const Hero = () => {
-  return (
+    return (
     <section className="hero-section">
-      <img src={heroImg} alt="LOOk Collection" className="hero-image" />
-      <div className="hero-content">
-        {/* El texto centrado ahora vive dentro del Hero */}
+        <img src={heroImg} alt="LOOk Collection" className="hero-image" />
+        <div className="hero-overlay">
         <ItemListContainer greeting="New Arrivals: Season 2025" />
-      </div>
+        </div>
     </section>
-  );
+    );
 };
 
 export default Hero;
